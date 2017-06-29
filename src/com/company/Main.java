b905f7c807c24598729fd491249147288fb04c0a
HK-RazieL <s.angelkov@abv.bg>
2017-06-30 02:35:34 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+
+          Integer peopleToTransport = scanner.nextInt();
+          Integer elevatorCapacity = scanner.nextInt();
+          Integer numberOfTrips = peopleToTransport / elevatorCapacity;
+
+          Integer result= (((int)Math.ceil((double)(peopleToTransport % elevatorCapacity)) > 0)
+                  ? numberOfTrips + 1
+                  : numberOfTrips);
+
+          System.out.print(result);
+
+    }
+}
